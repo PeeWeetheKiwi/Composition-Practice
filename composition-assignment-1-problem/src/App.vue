1078964c6d83426fdd6092e08dcdd5cad2b9b077
iiwal <iiwally26@gmail.com>
2025-07-31 16:03:25 -0500

    coding sucks

--- composition-assignment-1-problem/src/App.vue
+++ composition-assignment-1-problem/src/App.vue
@@ -2,9 +2,9 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-if="goalShown">{{ goal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="toggleGoal">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -12,10 +12,34 @@
   <!-- Task 4: Also solve the assignment with the Options API -->
 </template>
 
-<script>
-export default {
-  
+<script setup>
+import {ref, reactive, computed} from "vue";
+
+// const goal = ref('Be A Master Coder!');
+const goals = reactive({
+  goal: 'Be A Master Coder!',
+  goalShown: false
+});
+
+function toggleGoal() {
+  goals.goalShown = !goals.goalShown;
 }
+
+const goal = computed(() => goals.goal)
+const goalShown = computed(() => goals.goalShown)
+// export default {
+//   data() {
+//     return {
+//       goal: 'Be A Master Coder!',
+//       goalShown: false
+//     }
+//   },
+//   methods: {
+//     toggleGoal() {
+//       this.goalShown = !this.goalShown
+//     }
+//   }
+// }
 </script>
 
 <style>
